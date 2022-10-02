4276b51df9b7a5ba86b2d531893bb74aa580050f
BraedonWatkins <watkins.braedon@gmail.com>
2022-10-02 11:37:14 -0400

    root directory

---
+++ root/connect-usb.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# Definitions
+currUSB=/home/emurr/current-usb.txt
+preUSB=/home/emurr/previous-usb.txt
+diffUSB=/home/emurr/diff-usb.txt
+output=/dev/tty1
+
+
+# await race condition...
+echo "sleep 10s for race condition" > $output
+echo "look up more about polling... may need to implement instead of sleep" > $output
+for i in {1..10}
+do
+  echo -n "$i " > $output
+  sleep 1
+done
+
+# get usb directories (regex) & diff (comm -13) with what was last in the usb
+ls /dev/sd* > "$currUSB"
+comm -13 "$preUSB" "$currUSB" > "$diffUSB"
+
+# take the diff which should now be the curr dir of the USB
+directory=$(sed -n '2{p;q}' "$diffUSB")
+
+# mount the curr dir of the USB
+sudo mount "$directory" /media/usb
+
+# copy the mounted USB files to our NGINX dir
+sudo cp -r /media/usb/toursite /var/www/html/
---
+++ root/disconnect-usb.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# get state of USB to compare to on reconnect
+currUSB=$(ls /dev/sd*)
+preUSB=/home/emurr/previous-usb.txt
+echo "$currUSB" > "$preUSB"
+
+
+# shitty unmount
+comm -13 /home/emurr/previous-usb.txt /home/emurr/current-usb.txt > /home/emurr/diff-usb.txt
+directory=$(sed -n '2{p;q}' /home/emurr/diff-usb.txt)
+
+sudo umount "$directory"
+
+
